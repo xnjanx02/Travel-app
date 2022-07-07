50da2ccc28a80c1cf2da22db01cf32845494e1e4
xnjanx02 <anjana.jk009@gmail.com>
2022-07-07 23:41:05 +0530

    new component view-destinations added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { SearcgPlacesComponent } from './searcg-places/searcg-places.component';
+import { ViewDestinationsComponent } from './view-destinations/view-destinations.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationComponent,
-    SearcgPlacesComponent
+    SearcgPlacesComponent,
+    ViewDestinationsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/view-destinations/view-destinations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-destinations',
+  templateUrl: './view-destinations.component.html',
+  styleUrls: ['./view-destinations.component.css']
+})
+export class ViewDestinationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
